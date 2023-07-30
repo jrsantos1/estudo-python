bc0b4a245606f93c266248eee0ee7ee0ff68578d
Jhonatan Santos <jhonatanjonh@outlook.com>
2023-07-30 20:27:44 -0300

    add comentarios

--- flask/cadastro_exemplo/app.py
+++ flask/cadastro_exemplo/app.py
@@ -3,21 +3,29 @@ from flask import Flask, render_template, redirect, request, json, flash, url_fo
 import pyodbc
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, String, Integer
 import sqlite3
 import os
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'senha_secreta'
 
+
+# caminho do arquivo de banco de dados
 caminho_projeto = os.path.abspath(os.curdir)
 caminho_db = os.path.join(caminho_projeto, 'db.sqlite')
 
+# criando conexao sqlalchemy (orm do flask)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{caminho_db}'
 
+# configuracoes de login
 login_manager = LoginManager()
 login_manager.init_app(app=app)
+
+# instancia do sqlAchemy para criação dos modelos
 db = SQLAlchemy(app=app)
+
 
 def get_conexao():
     connection = sqlite3.connect(caminho_db)
@@ -25,11 +33,19 @@ def get_conexao():
     #connection_string = f'DRIVER={{SQLite3 ODBC Driver}};DATABASE={caminho_db};'
 
     return pyodbc.connect(connection_string)
+
+
+"""
+Login config
+"""
+
+#salvar usu[ario quando logar
 @login_manager.user_loader
 def load_user(user_id):
-    user = User(id=1, senha='123', racf='ribejho')
+    user = User.query.filter_by(id=user_id).first()
     return user
 
+#modelo do usuario no banco de dados
 class User(db.Model, UserMixin):
     id = Column(Integer, primary_key=True)
     racf = Column(String(100), nullable=False)
@@ -41,18 +57,18 @@ class User(db.Model, UserMixin):
         self.racf = racf
 
 
-
+# redirecionamento caso usuário não esteja logado
 @login_manager.unauthorized_handler
 def unauthorized():
-    # do stuff
     return redirect(url_for('login'))
 
-
+"""
+Cadastros
+"""
 @app.route('/')
 @login_required
 def home():
     return render_template('home.html', current_user=current_user)
-
 
 @app.route('/cadastrar', methods=['POST', 'GET'])
 def enviar():
@@ -74,32 +90,33 @@ def enviar():
         "ticker": ticker,
         "cnpj": cnpj,
         "tipo_ativo": tipo_ativo,
-       "valor_limite": r['valor_limite'],
-       "prazo_limite": r['prazo_limite'],
-       "tipo_limite": r['tipo_limite']
-     }  for _, r in df_limites.iterrows()]
+       "valor_limite": row['valor_limite'],
+       "prazo_limite": row['prazo_limite'],
+       "tipo_limite": row['tipo_limite']
+     } for _ , row in df_limites.iterrows()]
 
     df_cadastro = pd.DataFrame(data=data)
 
     try:
         with get_conexao() as con:
-            df = pd.read_sql(sql='select * from tgemissores', con=con)
-            state = 'insert into tgemissores (emissor, ticker, cnpj, tipo_ativo, valor_limite, prazo_limite, tipo_limite) values (?,?,?,?,?,?,?)'
-            for _, r in df_cadastro.iterrows():
-                parametros = [r['emissor'], r['ticker'], r['cnpj'], r['tipo_ativo'],
-                              r['valor_limite'], r['prazo_limite'], r['tipo_limite'],]
-            df_cadastro.to_sql('tgemissores', con=con, if_exists='append', index=False) #todo verificar depois, pois é mais eficiente
-            df = pd.read_sql(sql='select * from tgemissores', con=con)
+            df_cadastro.to_sql('tgemissores', con=con, if_exists='append', index=False)
     except Exception as e:
         print(e)
 
     return redirect(url_for('home'))
 
 @app.route('/cadastros')
+@login_required
 def cadastros():
     with get_conexao() as con:
-        df_cadastros = pd.read_sql(sql='select * from tgemissores', con=con)
+        racf = current_user.racf
+        df_cadastros = pd.read_sql(sql=f"select * from tgemissores where usuario <> '{racf}'", con=con)
     return render_template('cadastros.html', cadastros=df_cadastros.to_dict('records'))
+
+
+"""
+Login
+"""
 
 @app.route('/login')
 def login():
@@ -115,7 +132,10 @@ def logar():
 
     if racf and senha:
         user = User.query.filter_by(racf=racf).first()
-        login_user(user=user)
+        if check_password_hash(pwhash=user.senha, password=senha):
+            login_user(user=user)
+        else:
+            return redirect(url_for('login'))
     else:
         return redirect(url_for('login'))
     return redirect(url_for('home'))
@@ -127,6 +147,9 @@ def logout():
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
-    
+
+
+
+
     
     
